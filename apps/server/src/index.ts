aa6f97a528c33ef42fe88f4321849767c2096fad
Tim Schartman <timschartman@Tims-MacBook-Air.local>
2026-05-08 01:14:14 -0500

    Fix cors issues

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -29,6 +29,9 @@ async function start() {
   await fastify.register(fastifyCors, {
     origin: config.CORS_ORIGIN,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    strictPreflight: false,
   });
   await fastify.register(fastifyCookie);
   await fastify.register(fastifyRateLimit, {
